8f5e969665588e9d11fdfb2feaa4ea0f8151674a
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2018-12-07 07:17:17 -0600

    actualizado

--- public/js/map.js
+++ public/js/map.js
@@ -263,6 +263,8 @@ function initMap() {
         text: 'B'
       }
     });
+
+    
     drawingManager.setMap(map);
 
     google.maps.event.addListener(drawingManager, 'polygoncomplete', function(polygon) {
@@ -282,5 +284,38 @@ function initMap() {
       document.getElementById("dEdificio").setAttribute("style", "display: none;");
       document.getElementById("regEdificio").setAttribute("action", "/registraEdificio");
     });
-    
+    // To hide:
+drawingManager.setOptions({
+  drawingControl: false
+});
+
+// To show:
+drawingManager.setOptions({
+  drawingControl: true
+});
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 19.409620, lng: -99.316517},
+    zoom: 8
+  });
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
+    },
+    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+  drawingManager.setMap(map);
+}
 }
